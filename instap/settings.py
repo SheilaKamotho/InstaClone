ab6d62c70449414a836c83c841b964e66f0587df
Kamotho Sheila <kamothosheila@gmail.com>
2020-10-14 16:31:09 +0300

    add database

--- instap/settings.py
+++ instap/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'instap.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'insta',
+        'USER': 'sheila',
+    'PASSWORD':'kamo2211',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
