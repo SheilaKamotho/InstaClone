87823b3b9fe21a5ae9407e604c9ec864365e86bd
Kamotho Sheila <kamothosheila@gmail.com>
2020-10-18 19:05:42 +0300

    add editor and styling

--- insta/models.py
+++ insta/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
@@ -11,11 +12,17 @@ class Profile(models.Model):
     def save_profile(self):
         self.save()
 
+    @classmethod
+    def save_profile(cls):
+        posts=cls.objects.filter()
+        return posts
+
 class Image(models.Model):
     image = models.ImageField(upload_to = 'image/', null=True)
     name = models.CharField(max_length = 60)
     caption = models.TextField()
     likes = models.IntegerField()
+    editor = models.ForeignKey(User,on_delete=models.CASCADE, null=True)
     comments = models.TextField()
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
     pub_date = models.DateTimeField(auto_now_add=True, null=True)
@@ -25,5 +32,10 @@ class Image(models.Model):
 
     @classmethod
     def save_image(cls):
-        images=cls.objects.save()
-        return images
+        photos=cls.objects.filter()
+        return photos
+
+    @classmethod
+    def search_by_name(cls,search_term):
+        photos = cls.objects.filter(name__icontains=search_term)
+        return photos
--- insta/views.py
+++ insta/views.py
@@ -3,7 +3,28 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import Profile, Image
 
 # Create your views here.
-def posts(request):
-    return render(request, 'posts.html')
+@login_required(login_url='/accounts/login/')
+def photos(request):
+    posts=Profile.save_profile()
+    photos=Image.save_image()
+    return render(request, 'posts.html',{"photos":photos, "posts":posts})
+
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_name(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
+
+
+    
