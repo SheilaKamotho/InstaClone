0c27e1ad7f4d6e356adeac2bdf0ac4713df03398
Kamotho Sheila <kamothosheila@gmail.com>
2020-10-20 16:20:18 +0300

    add like function

--- insta/forms.py
+++ insta/forms.py
@@ -5,7 +5,7 @@ from .models import Image,Profile, Comment
 class NewImageForm(forms.ModelForm):
     class Meta:
         model = Image
-        exclude = ['editor','pub_date', 'likes','comment','profile']
+        exclude = ['editor','pub_date', 'likes','comment','profile','name']
         
 class NewProfileForm(forms.ModelForm):
     class Meta:
